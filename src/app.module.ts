84bca494a935864362815d601d0092c4c77a56dd
henrynguyen6677 <henrynguyen6677@gmail.com>
2025-05-03 10:39:48 +0700

    chore: generate favorites module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StoresModule } from './modules/stores/stores.module';
+import { FavoritesModule } from './modules/favorites/favorites.module';
 
 @Module({
-  imports: [StoresModule],
+  imports: [StoresModule, FavoritesModule],
   controllers: [AppController],
   providers: [AppService],
 })
