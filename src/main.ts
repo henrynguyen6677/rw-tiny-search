52b0621f6cbc7b790fbe74c358b377889934f1f6
henrynguyen6677 <henrynguyen6677@gmail.com>
2025-05-03 11:13:37 +0700

    chore: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+
+import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Tiny search engine')
+    .setDescription('Search engine for points of interest and other')
+    .setVersion('1.0')
+    .addTag('search')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
