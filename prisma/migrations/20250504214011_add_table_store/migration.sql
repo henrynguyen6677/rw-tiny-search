7732d9b7c65ac6f75f95358173f1115550f06dc3
henrynguyen6677 <henrynguyen6677@gmail.com>
2025-05-05 04:40:32 +0700

    feat: add store table

---
+++ prisma/migrations/20250504214011_add_table_store/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `Store` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `address` VARCHAR(191) NOT NULL,
+    `type` VARCHAR(191) NOT NULL,
+    `latitude` DOUBLE NOT NULL,
+    `longitude` DOUBLE NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
